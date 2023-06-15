c5281af3f1f7808a684567c9481412bb61e1f57a
iagosm <iagosousa201486@gmail.com>
2023-06-14 21:47:17 -0300

    att do projeto

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const db = require('./db/connection');
+const bodyParser = require('body-parser');
 
 const port = 3000;
 
@@ -8,16 +9,23 @@ app.listen(port, function(){
   console.log(`O express está rodando na porta ${port}`);
 })
 
+//Body parser
+app.use(bodyParser.urlencoded({ extended: false}));
+
 // Db Connection
-db.authenticate()
+db
+  .authenticate()
   .then(() => {
     console.log("Conectou ao banco com sucesso!");
   })
   .catch(err => {
-    console.log('Ocorreu um erro', err);
+    console.log('Ocorreu um er', err);
   })
 
 // Routes 
 app.get('/', (req, res) => {
-  res.send('está funcionando 5');
+  res.send('está funcionando 3');
 })
+
+//jobs routes
+app.use('/jobs', require('./routes/jobs'));
